79733ce9a94e19e093e1428e9431f77f2ae8d578
Bob Belderbos <bobbelderbos@gmail.com>
2024-05-03 14:57:58 +0200

    add model to admin check if entry already exist first tabs to spaces

--- blade/procData/views.py
+++ blade/procData/views.py
@@ -3,49 +3,43 @@ from procData.models import breachStructure, dbQueriedUsers
 from procData.forms import uploadCSV
 from csv import DictReader
 
+
 # Create your views here.
 def breachStructure_list(request):
-	breachStructures = breachStructure.objects.all()
+    breachStructures = breachStructure.objects.all()
 
-
-	return render(request,
-		'breachStructure/list.html',
-		{'breachStructures':breachStructures})
+    return render(
+        request, "breachStructure/list.html", {"breachStructures": breachStructures}
+    )
 
 
 def breachStructure_detail(request, email):
-	breachStruct = get_object_or_404(breachStructure, email=email)
+    breachStruct = get_object_or_404(breachStructure, email=email)
+
+    return render(
+        request, "breachStructure/detail.html", {"breachStructures": breachStruct}
+    )
 
 
-	return render(request,
-		'breachStructure/detail.html',
-		{'breachStructures':breachStruct})
-
-#def query- username
 def upload_csv(request):
-	if request.method == "POST":
-		form = uploadCSV(request.POST, request.FILES)
-		if form.is_valid():
-			csv_file = request.FILES['csv_file']
+    if request.method == "POST":
+        form = uploadCSV(request.POST, request.FILES)
+        if form.is_valid():
+            csv_file = request.FILES["csv_file"]
 
-			reader = DictReader(csv_file.read().decode('utf-8').splitlines())
-			for row in reader:
-				print(row)
-				#dbQueriedUsers.objects.create(user=row[0])
+            reader = DictReader(csv_file.read().decode("utf-8").splitlines())
+            for row in reader:
+                user, created = dbQueriedUsers.objects.get_or_create(user=row[0])
+                if not created:
+                    print(f"Skip, user {user} already exists")
 
-			return redirect('success_url')
+            return redirect("success_url")
 
+    else:
+        form = uploadCSV()
 
-	else:
-		form = uploadCSV()
+    return render(request, "csvUpload/csvUpload.html", {"form": form})
 
-
-	return render(request,
-		"csvUpload/csvUpload.html",
-		{"form":form}
-		)
 
 def success_url(request):
-	return render(request,
-		"csvUpload/success_url.html"
-		)
+    return render(request, "csvUpload/success_url.html")
