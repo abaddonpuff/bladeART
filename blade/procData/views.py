27b44250337e24a39a8f0cfcde972046aded47a6
abaddonpuff <abaddonpuff@gmail.com>
2024-05-06 21:37:25 -0400

    Modified script to test and tried to add a show website with the processed data from the CSV failing currently when trying to migrate.

--- blade/procData/views.py
+++ blade/procData/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from procData.models import breachStructure, dbQueriedUsers
 from procData.forms import uploadCSV
 from csv import DictReader
+from Scripts.githubLogParser import employee_to_github_test
 
+LDAP_SIMULATED = './Scripts/simulatedResults/employeeUniverse.json'
 
 # Create your views here.
 def breachStructure_list(request):
@@ -29,7 +31,8 @@ def upload_csv(request):
 
             reader = DictReader(csv_file.read().decode("utf-8").splitlines())
             for row in reader:
-                user, created = dbQueriedUsers.objects.get_or_create(user=row['Names'])
+                github_associated = employee_to_github_test(row['Names'],LDAP_SIMULATED)
+                user, created = dbQueriedUsers.objects.get_or_create(user=row['Names'].lower(),github_account=github_associated)
                 if not created:
                     print(f"Skip, user {user} already exists")
 
@@ -43,3 +46,11 @@ def upload_csv(request):
 
 def success_url(request):
     return render(request, "csvUpload/success_url.html")
+
+
+def show_allusers(request):
+    githubQueried = dbQueriedUsers.objects.all()
+
+    return render(
+        request, "csvUpload/list_users.html", {"dbQueriedUsers": githubQueried}
+    )
